d8cd4f9e2c94577fff693a96bc0cc9599f0fbfe2
Michael Adams <mtadams007@gmail.com>
2018-03-13 13:33:41 -0400

    background colors stay constant

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-const appear = document.getElementById('sectionOne');
-const appear2 = document.getElementById('sectionTwo');
-const appear3 = document.getElementById('sectionThree');
-const appear4 = document.getElementById('sectionFour');
+const section = document.getElementById('sectionOne');
+const section2 = document.getElementById('sectionTwo');
+const section3 = document.getElementById('sectionThree');
+const section4 = document.getElementById('sectionFour');
 
-
-// window.addEventListener('scroll', function(a){
-//   findShow(appear);
-// });
 
 let i = 0;
 let findShow = (arr) => {
@@ -16,36 +12,32 @@ let findShow = (arr) => {
   }
 }
 
-// window.addEventListener('scroll', function(a){
-//   for (let i = 0; i < appear.length; i++) {
-//     appear[i].classList.add('showAnimation');
-//   }
-// });
-
 
 window.addEventListener('scroll', function(a){
   if (window.scrollY > 0 && window.scrollY < 500 ) {
-      appear.classList.add('showAnimation');
-      appear.classList.remove('hidden');
-      appear2.classList.remove('showAnimation');
-      appear2.classList.add('hidden')
+      section.classList.add('showAnimation');
+      section.classList.remove('hidden');
+      section2.classList.remove('showAnimation');
+      section2.classList.add('hidden')
   } else if (window.scrollY > 500 && window.scrollY < 1400) {
-      appear2.classList.add('showAnimation');
-      appear2.classList.remove('hidden')
-      appear.classList.remove('showAnimation');
-      appear.classList.add('hidden');
+      section2.classList.add('showAnimation');
+      section2.classList.remove('hidden')
+      section.classList.remove('showAnimation');
+      section.classList.add('hidden');
   } else if (window.scrollY > 1400 && window.scrollY < 2000) {
-    appear3.classList.add('showAnimation');
-    appear3.classList.remove('hidden');
-    appear2.classList.remove('showAnimation');
-    appear2.classList.add('hidden');
-    appear4.classList.remove('showAnimation');
-    appear4.classList.add('hidden');
+    section3.classList.add('showAnimation');
+    section3.classList.remove('hidden');
+    section2.classList.remove('showAnimation');
+    section2.classList.add('hidden');
+    section4.classList.remove('showAnimation');
+    section4.classList.add('hidden');
 }
  else {
-    appear4.classList.add('showAnimation');
-    appear4.classList.remove('hidden');
-    appear3.classList.remove('showAnimation');
-    appear3.classList.add('hidden');
+    section4.classList.add('showAnimation');
+    section4.classList.remove('hidden');
+    section3.classList.remove('showAnimation');
+    section3.classList.add('hidden');
 }
 });
+
+// Better solution stolen from Kay
